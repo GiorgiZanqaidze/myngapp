912d81895b419222d5ac382e1890debcce6615ed
giorgizankaidze <giorgi.zankaidze@optio.ai>
2025-04-03 21:47:12 +0400

    feat: add base libs

--- libs/products/jest.config.ts
+++ libs/products/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
-  displayName: 'ui',
-  preset: '../jest.preset.js',
+  displayName: 'products',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/ui',
+  coverageDirectory: '../../coverage/libs/products',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- libs/products/src/lib/products/products.component.spec.ts
+++ libs/products/src/lib/products/products.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ButtonComponent } from './button.component';
+import { ProductsComponent } from './products.component';
 
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe('ProductsComponent', () => {
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent],
+      imports: [ProductsComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
